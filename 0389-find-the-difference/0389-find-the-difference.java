ab9bfa2486cb19dd6ef8bcbe6b70fd9433a04754
AYUSH RAI <86788413+raiayush2610@users.noreply.github.com>
2023-09-25 11:53:38 +0530

    Time: 12 ms (10.41%), Space: 40.5 MB (83.61%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -7,11 +7,8 @@ class Solution {
         HashMap<Character, Integer> mp2 = new HashMap<Character, Integer>();
         for(int i = 0; i < s.length(); i++){
             char c = s.charAt(i);
-            if(!mp1.containsKey(c)){
-                mp1.put(c, 1);
-            }else{
-                mp1.put(c, mp1.get(c) + 1);
-            }
+            mp1.put(c,mp1.getOrDefault(c,0)+1);
+            
         }
         for(int i = 0; i < t.length(); i++){
             char c = t.charAt(i);
