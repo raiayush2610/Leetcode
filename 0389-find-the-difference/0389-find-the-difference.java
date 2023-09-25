e1c6cfcce3b7733e1ffef2f212d56ce1af087739
AYUSH RAI <86788413+raiayush2610@users.noreply.github.com>
2023-09-25 11:47:27 +0530

    Time: 12 ms (10.41%), Space: 41.1 MB (20.11%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,12 +1,38 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-       int num = 0;
-        for(int ch : t.toCharArray()) {
-            num += ch;
+        if(s.length() == 0){
+            return t.charAt(0);
         }
-        for(int ch : s.toCharArray()) {
-            num -= ch;
+        HashMap<Character, Integer> mp1 = new HashMap<Character, Integer>();
+        HashMap<Character, Integer> mp2 = new HashMap<Character, Integer>();
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(!mp1.containsKey(c)){
+                mp1.put(c, 1);
+            }else{
+                mp1.put(c, mp1.get(c) + 1);
+            }
         }
-        return (char) (num);
+        for(int i = 0; i < t.length(); i++){
+            char c = t.charAt(i);
+            if(!mp2.containsKey(c)){
+                mp2.put(c, 1);
+            }else{
+                mp2.put(c, mp2.get(c) + 1);
+            }
+        }
+        char ans = ' ';
+        for(Map.Entry<Character, Integer>mp: mp2.entrySet()){
+            char c = mp.getKey();
+            int val = mp.getValue();
+            if(!mp1.containsKey(c)){
+                ans = c;
+                break;
+            }else if(mp1.containsKey(c) && mp1.get(c) != val){
+                ans = c;
+                break;
+            }
+        }
+        return ans;
     }
 }
