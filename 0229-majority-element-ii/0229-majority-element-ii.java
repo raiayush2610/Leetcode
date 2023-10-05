d4cb6c8325cbf736ad471123981b2a9852a97348
AYUSH RAI <86788413+raiayush2610@users.noreply.github.com>
2023-10-05 09:02:46 +0530

    Time: 11 ms (40.97%), Space: 46.1 MB (89.97%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -6,7 +6,7 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
         }
-            Set<Integer> keyset =map.keySet();
+           
             for(Integer key: map.keySet()){
                 if(map.get(key)>nums.length/3){
                     al.add(key);
