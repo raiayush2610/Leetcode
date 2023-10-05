ad5a5aba2906577ce7cda95d3e80f001cc3fd747
AYUSH RAI <86788413+raiayush2610@users.noreply.github.com>
2023-10-05 08:48:36 +0530

    Time: 11 ms (40.97%), Space: 46.6 MB (47.18%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -4,11 +4,7 @@ class Solution {
 
         ArrayList<Integer> al= new ArrayList<>();
         for(int i=0;i<nums.length;i++){
-            if(map.containsKey(nums[i])){
-                map.put(nums[i],map.get(nums[i])+1);
-            }else{
-                map.put(nums[i],1);
-            }
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
         }
             Set<Integer> keyset =map.keySet();
             for(Integer key: map.keySet()){
