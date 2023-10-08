5e37ff203724d84d6babf6e2f71a236c9c9a6fb8
AYUSH RAI <86788413+raiayush2610@users.noreply.github.com>
2023-10-08 18:24:45 +0530

    Time: 10 ms (81.94%), Space: 42.3 MB (91.67%) - LeetHub

---
+++ 1569-max-dot-product-of-two-subsequences/1569-max-dot-product-of-two-subsequences.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int maxDotProduct(int[] nums1, int[] nums2) {
+        int n= nums1.length;
+        int m= nums2.length;
+        int [][]dp = new int [n+1][m+1];
+        int ans= Integer.MAX_VALUE;
+        for(int i=0 ;i<n;i++){
+            for(int j=0;j<m;j++){
+                dp[i][j]=Integer.MIN_VALUE;
+            }
+        }
+        return dp(nums1, nums2, 0, 0, dp);
+        
+    }
+    private int dp(int[] nums1, int[] nums2, int i, int j, int[][] memo) {
+        int n = nums1.length;
+        int m = nums2.length;
+        
+        if (i == n || j == m) {
+            return Integer.MIN_VALUE;
+        }
+        
+        if (memo[i][j] != Integer.MIN_VALUE) {
+            return memo[i][j];
+        }
+        
+        memo[i][j] = Math.max(
+            nums1[i] * nums2[j] + Math.max(dp(nums1, nums2, i + 1, j + 1, memo), 0),
+            Math.max(dp(nums1, nums2, i + 1, j, memo), dp(nums1, nums2, i, j + 1, memo))
+        );
+        
+        return memo[i][j];
+    }
+}
