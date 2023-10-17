3b5ab6dcaab18a3288e0f1475e9ea66c41c97791
AYUSH RAI <86788413+raiayush2610@users.noreply.github.com>
2023-10-17 18:26:24 +0530

    Time: 5 ms (20.51%), Space: 44.2 MB (95.6%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -2,35 +2,27 @@ import java.util.Arrays;
 
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        // Get the sizes of both input arrays.
-        int n = nums1.length;
-        int m = nums2.length;
+     int n= nums1.length;
+     int m= nums2.length;
+    int [] newArray = new int [n+m];
+    int k=0;
+    for(int i= 0;i<n;i++){
+         newArray[k++]= nums1[i];
+    }
+    for(int i= 0;i<m;i++){
+         newArray[k++]= nums2[i];
+     }
+    Arrays.sort(newArray);
 
-        // Merge the arrays into a single sorted array.
-        int[] merged = new int[n + m];
-        int k = 0;
-        for (int i = 0; i < n; i++) {
-            merged[k] = nums1[i];
-            k++;
-        }
-        for (int i = 0; i < m; i++) {
-            merged[k++] = nums2[i];
-        }
+    int nl = newArray.length;
+    double mediam =0;
+    if(nl%2==0){
+        double num1=newArray[nl/2]+ newArray[(nl-1)/2];
+        return ( num1/2);
+    }else
+        return (double) newArray[nl/2];
+    
 
-        // Sort the merged array.
-        Arrays.sort(merged);
-
-        // Calculate the total number of elements in the merged array.
-        int total = merged.length;
-
-        if (total % 2 == 1) {
-            // If the total number of elements is odd, return the middle element as the median.
-            return (double) merged[total / 2];
-        } else {
-            // If the total number of elements is even, calculate the average of the two middle elements as the median.
-            int middle1 = merged[total / 2 - 1];
-            int middle2 = merged[total / 2];
-            return ((double) middle1 + (double) middle2) / 2.0;
-        }
+    
     }
 }
