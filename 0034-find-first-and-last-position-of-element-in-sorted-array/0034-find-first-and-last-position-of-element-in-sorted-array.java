e70b013482298c121c77f7b4e934f1287386e743
AYUSH RAI <86788413+raiayush2610@users.noreply.github.com>
2023-10-17 09:51:28 +0530

    Time: 0 ms (100%), Space: 44.8 MB (7.62%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -10,24 +10,24 @@ class Solution {
         }
     }
     public int findNumber(int []nums, int  target,boolean find){
-        int low = 0;
-        int high= nums.length-1;
+        int start = 0;
+        int end= nums.length-1;
         int index= -1;
         
-        while(low<=high){
-        int mid =  high+(low-high)/2;
+        while(start<=end){
+        int mid =  end+(start-end)/2;
 
             if(nums[mid]==target){
                 index= mid;
                 if(find){
-                    high=mid-1;
+                    end=mid-1;
                 }else{
-                    low=mid+1;
+                    start=mid+1;
                 }
             }else if (nums[mid]<target){
-                low=mid +1;
+                start=mid +1;
             }else{
-                high =mid -1;
+                end =mid -1;
             }
         }
         return index;
