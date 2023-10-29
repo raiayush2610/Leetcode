c358d2adb32860ff826d73280322ced524cad4c4
AYUSH RAI <86788413+raiayush2610@users.noreply.github.com>
2023-10-29 11:36:10 +0530

    Time: 7 ms (44.06%), Space: 43 MB (51.98%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,29 +1,35 @@
 class Solution {
-    public int solTab(int []prices,int k){
-        int n=prices.length;
-        int  curr[][]= new int [2][k+1];
-        int  next[][]= new int [2][k+1];
-        for(int i=n-1;i>=0;i--){
-            for(int buy =0;buy<=1;buy++){
-                for(int limit=1;limit<=k;limit++){
-                    int profit =0;
-                    if(buy==1){
-            int buykaro=-prices[i]+next[0][limit];
-            int skipkaro=0+next[1][limit];
+    public int  sol(int i,int on,int k,int []prices,int dp[][]){
+        if(i== prices.length){
+            return 0;
+        }
+        if(on ==2*k){
+            return 0;
+        }
+        if(dp[i][on]!=-1){
+            return dp[i][on];
+        }
+        int profit =0;
+        if(on %2 == 0){
+            // buy allowed
+            int buykaro=(-prices[i]+sol(i+1,on+1,k,prices,dp));
+            int skipkaro=0+sol(i+1,on,k,prices,dp);
             profit =Math.max(buykaro,skipkaro);
-        }else{
-            int sellkaro =prices[i]+next[1][limit-1];
-            int skipk= 0+next[0][limit];
+        }
+        else{
+            int sellkaro =(+prices[i]+sol(i+1,on+1,k,prices,dp));
+            int skipk= 0+sol(i+1,on,k,prices,dp);
             profit =Math.max(sellkaro,skipk);
         }
-        curr[buy][limit]=profit;
-        }
-    }
-    next=curr;
-    }
-       return  next[1][k];   
+        dp[i][on]=profit;
+        return dp[i][on];
     }
     public int maxProfit(int k, int[] prices) {
-        return solTab(prices,k);
+        int n=prices.length;
+        int dp [][] =new int [n][2*k];
+        for(int i=0;i<n;i++){
+            Arrays.fill(dp[i],-1);
+        }
+        return sol(0,0,k,prices,dp);
     }
 }
