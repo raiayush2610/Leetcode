1014801ae8fb9d69532e79aa60710dcea7f21e4d
AYUSH RAI <86788413+raiayush2610@users.noreply.github.com>
2023-10-29 09:02:07 +0530

    Time: 1 ms (86.05%), Space: 44.3 MB (47.91%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -7,7 +7,7 @@ class Solution {
             }else{
                 maxprofit += 0;
             }
-        
+            // profit += prices[i] > prices[i-1] ? prices[i]-prices[i-1] : 0;
         }
         return maxprofit;
     } 
